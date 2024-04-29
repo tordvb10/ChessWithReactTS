bbf8e1e6a55421df70618317dfbfe4673deeeb84
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-29 11:16:00 +0200

    tailwind is checked for working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,9 +8,10 @@ function App() {
 
   return (
     <>
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>    </>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
 
