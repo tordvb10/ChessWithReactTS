9d7eca26ad30d2dd6230dd66fcaa5bb404ccf778
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-29 13:05:25 +0200

    getting there

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-
+import Board from './components/Board/Board.tsx'
 function App() {
  // const [count, setCount] = useState(0)
 
@@ -11,6 +11,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Board />
     </>
   )
 }
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,10 @@
 //import BoardPiece from "../BoardPiece/BoardPiece.tsx";
+import {data as DataArray} from "../../Data/Board/BoardData.json"
 export default function Board(){
-
+    console.log(DataArray)
+    return (
+        <>
+            
+        </>
+    )
 }
