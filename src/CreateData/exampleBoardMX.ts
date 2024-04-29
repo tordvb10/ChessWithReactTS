0569b1346d7505d254845184bf407e07cf1047c2
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-29 12:06:40 +0200

    ts-node

---
+++ src/CreateData/exampleBoardMX.ts
@@ -0,0 +1,13 @@
+import fs from "fs"
+
+const BoardArray = () => {
+    let piece = [""]
+    console.log(piece)
+    for (let i = 0; i< 8;i++){
+        console.log(i)
+    }
+}
+
+console.log(BoardArray)
+
+fs.writeFileSync
