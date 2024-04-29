66b33a54e3774ef8f7e13c0c73c0ace9e16077e5
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-29 14:45:20 +0200

    ordnet med first svg i json

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,6 +3,7 @@ interface BoardPieceInterFace {
     letter: String;
     number: String;
 }
+import {StrictMode} from "react";
 import {data as BoardData} from "../../Data/Board/BoardData.json"
 import styleBoard from "./Board.module.css"
 export default function Board(){
@@ -10,18 +11,16 @@ export default function Board(){
         console.log(BoardPiece)
     }
     const BoardArray = BoardData.BoardArray;
-    console.log(BoardArray)
     return (
-        <>
-        <h1>This is Board</h1>
-            <div className="GridContainer grid grid-cols-8">
+        <StrictMode>
+            <h1>This is Board</h1>
+            <div key="Board" className="GridContainer grid grid-cols-8">
                 {
                     BoardArray.map((BoardField)=>{
                         return(
-                            <>
+                            <StrictMode key={BoardField[0].letter}>
                                 {
                                     BoardField.map((BoardPiece)=>{
-                                        console.log(BoardPiece)
                                         return(
                                             <button onClick={()=>{pieceClicked(BoardPiece)}} className={styleBoard.BoardPiece} key={`${BoardPiece.letter}${BoardPiece.number}`}>
                                                 {BoardPiece.letter}{BoardPiece.number}
@@ -29,11 +28,11 @@ export default function Board(){
                                         )
                                     })
                                 }
-                            </>
+                            </StrictMode>
                         )
                     })
                 }
             </div>
-        </>
+        </StrictMode>
     )
 }
