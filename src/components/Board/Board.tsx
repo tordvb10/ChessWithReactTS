3915e341491f163ead0226b3a38fbe6118d45b6a
tordvb10 <tordvb10.kodehode@gmail.com>
2024-04-29 13:59:59 +0200

    brett laget

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,29 +1,32 @@
 //import BoardPiece from "../BoardPiece/BoardPiece.tsx";
 import {data as BoardData} from "../../Data/Board/BoardData.json"
+import styleBoard from "./Board.module.css"
 export default function Board(){
     const BoardArray = BoardData.BoardArray;
     console.log(BoardArray)
     return (
         <>
-            <h1>This is Board</h1>
-            {
-                BoardArray.map((BoardField)=>{
-                    return(
-                        <>
-                            {
-                                BoardField.map((BoardPiece)=>{
-                                    console.log(BoardPiece)
-                                    return(
-                                        <div>
-                                            {BoardPiece.letter}
-                                        </div>
-                                    )
-                                })
-                            }
-                        </>
-                    )
-                })
-            }
+        <h1>This is Board</h1>
+            <div className="GridContainer grid grid-cols-8">
+                {
+                    BoardArray.map((BoardField)=>{
+                        return(
+                            <>
+                                {
+                                    BoardField.map((BoardPiece)=>{
+                                        console.log(BoardPiece)
+                                        return(
+                                            <div className={styleBoard.BoardPiece} key={`${BoardPiece.letter}${BoardPiece.number}`}>
+                                                {BoardPiece.letter}{BoardPiece.number}
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
